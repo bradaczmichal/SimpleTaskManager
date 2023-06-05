d6db5fcdecf7f58ab00b593c6e42f2527008e9e5
Michał Bradacz <bm49012@zut.edu.pl>
2023-06-05 15:58:48 +0200

    Connected to Azure SQL Database and create first user and read into table.

--- XamarinTest/SignUpPage.xaml.cs
+++ XamarinTest/SignUpPage.xaml.cs
@@ -16,7 +16,7 @@ namespace XamarinTest
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SignUpPage : ContentPage
 	{
-
+        private readonly SqlConnection ConnectionString = new SqlConnection(@"Server=tcp:mainsrv.database.windows.net,1433;Initial Catalog=Database;Persist Security Info=False;User ID=adminsql;Password=Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         public SignUpPage ()
 		{
 			InitializeComponent ();
@@ -65,7 +65,7 @@ namespace XamarinTest
             try
             {
                 ConnectionString.Open();
-                SqlCommand insert = new SqlCommand("INSERT INTO [User] VALUES (@FirstName, @LastName, @Username, @Password", ConnectionString);
+                SqlCommand insert = new SqlCommand("INSERT INTO [Users] VALUES (@FirstName, @LastName, @Username, @Password)", ConnectionString);
                 insert.Parameters.AddWithValue("@FirstName", firstName);
                 insert.Parameters.AddWithValue("@LastName", lastName);
                 insert.Parameters.AddWithValue("@Username", username);
